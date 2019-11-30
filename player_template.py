769d1fe430ccadaaabc6c2df1db3ce8257bef51e
Aarsh Oza <aarsh@localhost.localdomain>
2019-11-29 19:01:05 -0500

    Last card method changes

--- player_template.py
+++ player_template.py
@@ -25,7 +25,7 @@ def get_new_deck():
         for rank in ranks:
             temp.append(rank + suit)
         deck.append(temp)
-    print(deck)
+    # print(deck)
     return deck
 
 
@@ -33,18 +33,23 @@ def get_random_card_to_play(hand):
     return random.choice(hand)
 
 
-def is_lead_suit_exists(my_hand, first_card_of_trick: str):
+def is_lead_suit_exists(my_hand, first_card_of_trick: str): # what is the use of this function
     return any(first_card_of_trick[1] in card[1] for card in my_hand)
 
 
 def get_card_to_play_two_three(suit, trick, hand, state):
     result = ''
-
-    if not hand[suit]:
+    suit_in_play = trick[0][1]
+    if suit_in_play not in hand:
         pass
     else:
-        cards_available_to_play = hand[suit]
-        if cards_available_to_play[0] == state[suits.index(suit)][0]:
+        cards_available_to_play = []
+        for v_arr in hand[suit_in_play]:
+            if isinstance(v_arr, list):
+                cards_available_to_play.append(v_arr[0])
+            else:
+                cards_available_to_play.append(v_arr)
+        if cards_available_to_play[0] == state[suits.index(suit)][0]:   # If state does not contain our cards then why comparing cards_available_to_play with state
             result = cards_available_to_play[0]
         else:
             temp = len(cards_available_to_play)
@@ -61,10 +66,20 @@ def get_card_to_play_last(trick, hand):
     for card in trick:
         if ranks.index(card[0]) > ranks.index(highest_rank_in_play):
             highest_rank_in_play = card[0]
-    cards_can_be_played = hand[suit_in_play]
-    rank_cards_to_be_played = [ranks.index(card[0]) for card in cards_can_be_played]
 
-    if len(cards_can_be_played) > 0:
+
+    if suit_in_play  in hand:
+
+        cards_can_be_played =[]
+
+        for v_arr in  hand[suit_in_play]:
+            if isinstance(v_arr, list):
+                cards_can_be_played.append(v_arr[0])
+            else:
+                cards_can_be_played.append(v_arr)
+
+        rank_cards_to_be_played = [ranks.index(card[0]) for card in cards_can_be_played]
+
         higher_rank_cards_to_be_played = [card_rank for card_rank in rank_cards_to_be_played if
                                           card_rank > ranks.index(highest_rank_in_play)]
         if len(higher_rank_cards_to_be_played) > 0:
@@ -72,7 +87,14 @@ def get_card_to_play_last(trick, hand):
         else:
             return ranks[min(rank_cards_to_be_played)] + suit_in_play
     else:
-        remaining_non_suit_cards = [v for v_arr in hand.values() for v in v_arr]
+        remaining_non_suit_cards = []
+        for v_arr in hand.values():
+             for v in v_arr:
+                 if isinstance(v, list):
+                    remaining_non_suit_cards.append(v[0])
+                 else:
+                     remaining_non_suit_cards.append(v)
+
         min_rank = min([ranks.index(card[0]) for card in remaining_non_suit_cards])
         likely_cards = [card for card in remaining_non_suit_cards if ranks.index(card[0]) == min_rank]
         random.shuffle(likely_cards)
@@ -103,7 +125,6 @@ def get_card_to_play_first(my_hand, state):
             if weights[index] in suit:
                 return False
         return True
-
     # sort cards suit wise and rank wise
     # e.g. sorted_cards = [['AS', 'KS', '8S'], ['7H', '4H'], ['8D', '5D', '3D'], ['4C', '2C']]
     sorted_cards = sort_cards()
@@ -112,12 +133,12 @@ def get_card_to_play_first(my_hand, state):
         if len(suit) > 0:
             card = suit[0]
             if is_card_biggest_in_suit(card, state):
-                return card
+                return card # The Function  will not proceed and even if we have Ace and king of Diamonds it will start with 10 of hearts
 
-    suit_lengths = [len(suit) for suit in sorted_cards]
+    suit_lengths = [len(suit) for suit in sorted_cards] #list of lengths of suits
     max_length_suit = max(suit_lengths)
-    max_length_suit_indices = [index for index, length in enumerate(suit_lengths) if length == max_length_suit]
-    smallest = 0
+    max_length_suit_indices = [index for index, length in enumerate(suit_lengths) if length == max_length_suit] #index of list having maximum elements in list
+    smallest = 0    #why not 1
     card_to_return = sorted_cards[max_length_suit_indices[0]][0]
     for index in max_length_suit_indices:
         card = sorted_cards[index][max_length_suit-1]
@@ -129,27 +150,27 @@ def get_card_to_play_first(my_hand, state):
 
 
 def get_card_to_play(trick, my_hand, state):
-    hand = dict()
+    hand = dict() #hand has keys of suit and values is the list of cards available
     result = ''
 
-    for x in my_hand:
+    for x in my_hand:   #   my_hand = ["2C", "4C", "6C", "8C", "TC", "JC", "KC", "2S", "9S", "TS", "KS", "AS", "TD"] #why for loop
         suit = x[1]
-        if suit in hand.keys():
-            hand[suit].append([x])
+        if suit in hand.keys():     #If suit is present in hand than append in list else assign the suit to hand list
+            hand[suit].append([x])  #why this
         else:
             hand[suit] = [x]
 
-    if len(trick) == 0:
-        if len(my_hand):
+    if len(trick) == 0: # length of trick is 0 so that  means ours is first chance
+        if len(my_hand):    # If we have cards in hand
             result = get_card_to_play_first(my_hand, state)
 
-    elif len(trick) == 1 or len(trick) == 2:
+    elif len(trick) == 1 or len(trick) == 2:    # Length of trick is 1 we have second chance and if length of trick is 2 we have third chance
         first_card = trick[0]
         first_suit = first_card[1]
 
         result = get_card_to_play_two_three(first_suit, trick, hand, state)
 
-    elif len(trick) == 3:
+    elif len(trick) == 3:   # If length of trick is 3 we have last chance
         result = get_card_to_play_last(trick, hand)
 
     return result
@@ -222,7 +243,7 @@ class Player(object):
         """
         for i in trick:
             suit = i[1]
-            self.state[suit].remove(i)
+            self.state[suit].remove(i)  #do not know
 
         self.played_cards = self.played_cards + trick
         self.point[winner] = self.point[winner] + 1
